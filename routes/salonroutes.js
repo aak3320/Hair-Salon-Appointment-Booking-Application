99f12270d808ca2185bf47a37c1b95d075760789
aak3320 <aakashcs2110@email.com>
2026-05-13 21:28:51 +1000

    Aakash helped me pushed code due to insuffecient storage in her PC.

--- routes/salonroutes.js
+++ routes/salonroutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
 const salonController = require("../controllers/salonController");
 
+router.get("/", salonController.getAllSalons);
 router.get("/featured", salonController.getFeaturedSalons);
+router.get("/nearby", salonController.getNearbySalons);
 
 module.exports = router;
