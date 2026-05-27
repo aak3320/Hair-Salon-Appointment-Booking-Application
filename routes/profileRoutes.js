c19cfbdf4772185583e9b4b8763936ad15142e22
aak3320 <aakashcs2110@gmail.com>
2026-05-27 21:05:45 +1000

    Conflict Resloved for middlewear.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { Profile, updateProfile, deleteAccont } = require("../controllers/profileController");
-const {protect} = require("../middleware/authMiddleware"); 
+const { protect } = require("../middleware/authMiddleware");
+const { deleteProfile } = require("../controllers/profileController");
 
 router.get("/", protect, Profile);
 
@@ -10,6 +11,7 @@ router.get("/", protect, Profile);
 router.put("/", protect, updateProfile);
 
 // Delete user account (DELETE api/profile)
-router.delete("/", protect, deleteAccount);
+
+router.delete("/", protect, deleteProfile);
 
 module.exports = router;
