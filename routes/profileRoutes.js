066ae6a92a837e3ceaeb5a459dfde01445684868
aak3320 <aakashcs2110@gmail.com>
2026-05-27 15:32:01 +1000

    Add DELETE /api/profile route to allow logged in user to delete their account

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { Profile, updateProfile } = require("../controllers/profileController");
+const { Profile, updateProfile, deleteProfile } = require("../controllers/profileController");
 const {protect} = require("../middleware/authMiddleware"); 
 
 router.get("/", protect, Profile);
 
 // Update logged in user profile (PUT /api/profile)
 router.put("/", protect, updateProfile);
+
+// Delete user account (DELETE api/profile)
+router.delete("/", protect, deleteProfile);
 
 module.exports = router;
