cfa59647e13955240495723a1103abd1bf2c13b8
krishamakwana11 <108140131+krishamakwana11@users.noreply.github.com>
2026-05-27 20:58:21 +1000

    Resolve some basic routes issues

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const appointmentController = require("../controllers/appointmentController");
-const protect = require("../middleware/authMiddleware");
+const {protect} = require("../middleware/authMiddleware");
 
 router.post("/", protect, appointmentController.createAppointment);
 router.patch("/:id/cancel", protect, appointmentController.cancelAppointment);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const reviewController = require("../controllers/reviewController");
-const verifyToken = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 
 // Create Review
-router.post("/", verifyToken, reviewController.createReview);
+router.post("/", protect, reviewController.createReview);
 
 // Get Reviews By Salon
 router.get("/", reviewController.getReviewsBySalon);
